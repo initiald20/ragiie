48d6f5de8c65893eb613e3989feab57dc034cff5
Dean Ding <func-dev-06@signify.com>
2024-09-26 17:05:10 +0800

    fix:lose mapper explain

--- src/main/java/com/ding/raggiestart/mapper/SetmealDishMapper.java
+++ src/main/java/com/ding/raggiestart/mapper/SetmealDishMapper.java
@@ -2,6 +2,8 @@ package com.ding.raggiestart.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.ding.raggiestart.entity.SetmealDish;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface SetmealDishMapper extends BaseMapper<SetmealDish> {
 }
